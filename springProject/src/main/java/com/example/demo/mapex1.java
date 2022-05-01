d27dc1245d9ebb118ae95f0563b296a98184277d
Chung woo jin <wjchang8028@naver.com>
2022-05-01 23:10:22 +0900

    map 데이터 삽입

---
+++ springProject/src/main/java/com/example/demo/mapex1.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import java.util.HashMap;
+
+public class mapex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("¹ä", 1000);
+		map.put("±èÄ¡", 500);
+
+	}
+
+}
