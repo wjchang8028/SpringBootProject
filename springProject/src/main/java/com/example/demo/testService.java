502ff84f23d9e20627ba371b4a07a8cd175bb689
Chung woo jin <wjchang8028@naver.com>
2022-05-08 01:53:15 +0900

    test페이지

--- springProject/src/main/java/com/example/demo/testService.java
+++ springProject/src/main/java/com/example/demo/testService.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class testService {
+
+	@Autowired
+	testMapper testmapper;
+
+//	public testService();
 
 }
