fa91bc96264b4af8f00ea3b3a8b69d45a4dfa135
Chung woo jin <wjchang8028@naver.com>
2022-05-18 23:03:29 +0900

    delete file

---
+++ project/src/main/java/spring/project/app/ProjectApplication.java
@@ -0,0 +1,29 @@
+package spring.project.app;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/*.xml");
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
+}
