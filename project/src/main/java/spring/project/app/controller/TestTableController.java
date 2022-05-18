47b212e94eb47de2b06e062a3e29a2f89efa04e1
Chung woo jin <wjchang8028@naver.com>
2022-05-18 23:05:04 +0900

    컨트롤러

---
+++ project/src/main/java/spring/project/app/controller/TestTableController.java
@@ -0,0 +1,26 @@
+package spring.project.app.controller;
+
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.project.app.service.TestTableService;
+
+@Controller
+public class TestTableController {
+
+	@Resource
+	private TestTableService testtableservice;
+
+	@RequestMapping(value = "/list.do")
+	public String view(Model model) throws Exception {
+		List<Map<String, Object>> AllList = testtableservice.SelectAllList();
+		System.out.println(AllList);
+		model.addAttribute("list", AllList);
+
+		return "list";
+	}
+}
