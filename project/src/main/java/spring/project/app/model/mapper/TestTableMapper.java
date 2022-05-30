1cadb13f1fae763f11b7fefbedc22f595ae38b5c
wjchu <wjchang8028@naver.com>
2022-05-30 22:19:42 +0900

    매퍼 checkID

--- project/src/main/java/spring/project/app/model/mapper/TestTableMapper.java
+++ project/src/main/java/spring/project/app/model/mapper/TestTableMapper.java
@@ -9,5 +9,7 @@ public interface TestTableMapper {
 
 // select * from Test_Table
 	public List<Map<String, Object>> SelectAllList() throws Exception;
+	
+	public Integer SelectCheckID(String userid) throws Exception;
 
 }
