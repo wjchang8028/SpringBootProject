851aaaf81b247617f51cd98b143d85920185e29a
wjchu <wjchang8028@naver.com>
2022-06-01 14:00:47 +0900

    API 커넥션 메소드 추가

--- project/src/main/java/spring/project/app/model/common/commonUtils.java
+++ project/src/main/java/spring/project/app/model/common/commonUtils.java
@@ -1,5 +1,63 @@
 package spring.project.app.model.common;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class commonUtils {
+
+	/**
+	 * API 커넥션 메소드
+	 * 
+	 * @param
+	 */
+	public String connectToServer(String data, String reqUrl) throws Exception {
+		HttpURLConnection conn = null;
+		BufferedReader resultReader = null;
+		PrintWriter pw = null;
+		URL url = null;
+
+		int statusCode = 0;
+		StringBuffer recvBuffer = new StringBuffer();
+		try {
+			url = new URL(reqUrl);
+			conn = (HttpURLConnection) url.openConnection(); // url커넥션 오픈설정
+			conn.setRequestMethod("POST");// 전송방식 설정
+			conn.setDoOutput(true);
+
+			pw = new PrintWriter(conn.getOutputStream());
+			pw.write(data);
+			pw.flush();
+
+			statusCode = conn.getResponseCode();
+
+			resultReader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			for (String temp; (temp = resultReader.readLine()) != null;) {
+				recvBuffer.append(temp).append("\n");
+			}
+
+			if (!(statusCode == HttpURLConnection.HTTP_OK)) {
+				System.out.println("statusCode httpok와 다른 오류");
+			}
+
+			System.out.println("리턴 데이터 값 : " + recvBuffer.toString().trim());
+
+			return recvBuffer.toString().trim();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "ERROR";
+		}
+
+	}
 
 }
