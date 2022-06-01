f29e5d7119c523bf9e19310fbb4ee7f6ee12fe58
wjchu <wjchang8028@naver.com>
2022-06-01 14:03:02 +0900

    SHA256 암호화 클래스 추가

---
+++ project/src/main/java/spring/project/app/model/common/commonEncrypt.java
@@ -0,0 +1,60 @@
+package spring.project.app.model.common;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class commonEncrypt {
+	/**
+	 * SHA암호화 생성 메소드
+	 * 
+	 * @param
+	 */
+	public String encrypt(String text) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(text.getBytes());
+
+		return byteToHex(md.digest());
+	}
+
+	/**
+	 * byte배열 -> 해시화 메소드
+	 * 
+	 * @param
+	 */
+
+	public String byteToHex(byte[] bytes) {
+		StringBuilder builder = new StringBuilder();
+		for (byte b : bytes) {
+			builder.append(String.format("%02x", b));
+		}
+
+		return builder.toString();
+	}
+
+	/**
+	 * SHA256 해시암호화 메소드
+	 * 
+	 * @param
+	 */
+	public String encryptSHA256(String strData) {
+		String strOUTData = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(strData.getBytes());
+
+			byte[] md5Sig = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < md5Sig.length; i++) {
+				String hex = Integer.toHexString(0xff & md5Sig[i]);
+				if (hex.length() == 1)
+					sb.append('0');
+				sb.append(hex);
+			}
+			strOUTData = sb.toString();
+		} catch (Exception e) {
+			System.out.print(e.toString());
+		}
+		System.out.println(strOUTData);
+		return strOUTData;
+	}
+}
