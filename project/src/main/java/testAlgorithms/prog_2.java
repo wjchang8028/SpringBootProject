28854881cc5face7e3e5138466e99fbdd2db06b5
wjchu <wjchang8028@naver.com>
2022-07-26 22:30:45 +0900

    2차원배열

--- project/src/main/java/testAlgorithms/prog_2.java
+++ project/src/main/java/testAlgorithms/prog_2.java
@@ -10,7 +10,7 @@ public class prog_2 {
 	public int[][] solution(int[][] arr1, int[][] arr2) {
 		int[][] answer = new int[arr1.length][arr2.length];
 
-		for (int i = 0; i < arr1.length; i++) {
+		for (int i = 0; i < arr1.length; i++) { //2중
 			for (int j = 0; j < arr1[i].length; j++) {
 
 				answer[i][j] = arr1[i][j] + arr2[i][j];
