5acccd232ec7cce2119f30102076918d742b677a
wjchu <wjchang8028@naver.com>
2022-08-07 15:17:10 +0900

    체크 컨트롤러

--- project/src/main/java/spring/project/app/controller/LoginController.java
+++ project/src/main/java/spring/project/app/controller/LoginController.java
@@ -1,8 +1,53 @@
 package spring.project.app.controller;
 
+import java.util.HashMap;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import spring.project.app.service.TestTableService;
 
 @RequestMapping
 public class LoginController {
+
+	@Resource
+	TestTableService testtableservice;
+
+	@RequestMapping("/validationID.do")
+	@ResponseBody
+	public String checkID(String userid) {
+		System.out.println("입력받은 값 : " + userid);
+
+		String checkid = testtableservice.selectCheckID(userid);
+		System.out.println("checkid " + Integer.parseInt(checkid));
+		if (Integer.parseInt(checkid) >= 1) {
+			return "FALSE";
+		} else {
+			return "TRUE";
+		}
+	}
+
+	@RequestMapping("validationPW.do")
+	@ResponseBody
+	public String checkPW(HttpServletRequest request) {
+
+		commonUtils utils = new commonUtils();
+
+		HashMap paramMap = new HashMap();
+
+		paramMap = utils.apiInit(request);
+		System.out.println(paramMap);
+
+		if (paramMap.get("userpw").equals(paramMap.get("checkuserpw"))) {
+			return "TRUE";
+		} else {
+
+			return "FALSE";
+		}
+
+	}
 
 }
