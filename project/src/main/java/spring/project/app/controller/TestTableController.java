904ca9b8890f31a56e35f3d09fb704c83b47f7d9
wjchu <wjchang8028@naver.com>
2022-09-16 23:52:05 +0900

    import 삭제

--- project/src/main/java/spring/project/app/controller/TestTableController.java
+++ project/src/main/java/spring/project/app/controller/TestTableController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import spring.project.app.service.TestTableService;
@@ -59,6 +58,8 @@ public class TestTableController {
 
 	@RequestMapping(value = "/apitest.do") // api컨트롤러 추가
 	public String page() {
+		
+	
 		return "apitest";
 	}
 
